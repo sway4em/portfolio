6791a09019474484b69e611ba797530e5d25e871
Swayam Chidrawar <schidraw@calpoly.edu>
2023-12-31 00:43:09 -0800

    updated config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ebe8f6b9204a7865"; }
+/******/ 		__webpack_require__.h = function() { return "0a24404a285f3912"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
